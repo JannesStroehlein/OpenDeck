709e734b5c183e43bee6dac35db251e6761c6cf9
Jannes Ströhlein <jati.stroehlein@gmail.com>
2021-02-16 10:33:24 +0100

    Restructure

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -13,10 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace OpenDeck.App
+namespace OpenDeck.Server
 {
     /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
